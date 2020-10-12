1bd55350b55c22144e78ce15cb0c47c3a1c9bc03
= <nguph@vng.com.vn>
2020-10-13 00:36:10 +0700

    build terraform

--- dev-build.sh
+++ dev-build.sh
@@ -2,14 +2,14 @@
 
 set -eo pipefail
 
-IMAGE_NAME="zenika/terraform-aws-cli:dev"
+IMAGE_NAME="nguhuphan/terraform-aws-cli"
 
 # Lint Dockerfile
 echo "Linting Dockerfile..."
 docker run --rm -i hadolint/hadolint:v1.17.6-alpine < Dockerfile
 echo "Lint Successful!"
 
-# Build dev image
+# Build image
 if [ -n "$1" ] && [ -n "$2" ] ; then
   echo "Building images with parameters AWS_CLI_VERSION=${1} and TERRAFORM_VERSION=${2}..."
   docker image build --build-arg AWS_CLI_VERSION="$1" --build-arg TERRAFORM_VERSION="$2" -t $IMAGE_NAME .
